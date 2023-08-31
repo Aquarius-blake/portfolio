8ddfc11c45e745ca738a500779791906fc100f5d
Aquarius-blake <danbaid72@gmail.com>
2023-08-31 10:40:31 +0000

    update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -141,7 +141,7 @@ const contactConfig = {
     description: "Looking forward to hearing from you!",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_SERVICE_ID: "service_5v3if9p",
     YOUR_TEMPLATE_ID: "TEMPLATE_ID",
     YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
 };
